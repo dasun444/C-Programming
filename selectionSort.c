b2d8174cc8191ac54acd5fa6505c66ef6989dc76
WW <ww@example.com>
2026-09-15 23:40:00 +0530

    Sorting - Selection Sort

---
+++ selectionSort.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+void selectionSort(int arr[],int n){
+    int i,j,minI,temp;
+    for (i = 0;i<n-1;i++){
+        minI=i;
+        for (j=i+1 ; j<n;j++){
+            if(arr[j]<arr[minI]){
+                minI = j ;
+            }
+        }
+
+        temp= arr[i];
+        arr[i]=arr[minI];
+        arr[minI]=temp;
+    }
+
+}
+
+
+
+
+int main (){
+
+    int arr[]={7,4,5,9,8,2,1};
+    //int length =sizeof(arr)/sizeof(int);
+    int n =8;
+    printf("UnSorted array  \n");
+    for(int i =0;i<n-1;i++){
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+    selectionSort(arr,n);
+    printf("Sorted array - Selection sort\n");
+    for(int i =0;i<n-1;i++){
+        printf("%d ",arr[i]);
+    }
+
+    return 0;
+}
